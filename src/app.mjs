b0b1aea59e8e34ebde5d0d13036c7566343dcea6
haileydemark11 <hdemark@smu.edu>
2026-02-24 12:41:25 -0600

    last class updates

--- src/app.mjs
+++ src/app.mjs
@@ -3,9 +3,11 @@ import express from 'express';
 const app = express(); 
 const port = 3000; 
 
-app.get('/', (request, response) => {
-    response.send("Hello, World!"); 
-}); 
+app.use(express.static('public')); 
+
+// app.get('/', (request, response) => {
+//     response.send("Hello, World!"); 
+// }); 
 
 app.listen(port, () => {
     console.log(`Application listening at port ${port}`);   // using the ` ` is another way of using a string
